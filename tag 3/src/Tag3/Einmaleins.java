469b005754149a8022da2aa5cecc5c9eec372ec8
Hamid-85 <kl.hamid85@gmail.com>
2021-08-30 14:43:33 +0200

    Einmaleins

---
+++ tag 3/src/Tag3/Einmaleins.java
@@ -0,0 +1,17 @@
+package Tag3;
+
+public class Einmaleins {
+
+	public static void main(String[] args) {
+		int a,b;
+		for(a=1;a<=10;a++) {
+		   for(b=1;b<=10;b++) 
+			   
+		  	  System.out.printf("%3dx%2d=%3d",a,b,a*b);
+		   System.out.println("");
+		  
+		
+		
+		}
+}
+}
